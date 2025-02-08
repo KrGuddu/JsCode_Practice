660e9a6e4119ec33e1e4a7ab3840b3fe20438b94
guddu kumar <146803696+KrGuddu@users.noreply.github.com>
2025-02-08 08:08:59 +0000

    02_array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -21,9 +21,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Guddu"))
-console.log(Array.from("Guddu"))
-console.log(Array.from({name: "Guddu"})) // interesting
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
