b56c8ec368c19044b573603e6d80012c02f1df5b
guddu kumar <146803696+KrGuddu@users.noreply.github.com>
2025-03-28 22:26:47 +0530

    02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -2,7 +2,7 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)        // o/p 
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
