926a9eb4dc052ad776a9c94a2908c09ae4005c35
guddu kumar <146803696+KrGuddu@users.noreply.github.com>
2026-05-10 14:47:18 +0000

    updates

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -2,6 +2,8 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
+
+//channing: To use multiple methods in a variables
 const newNums = myNumers
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
